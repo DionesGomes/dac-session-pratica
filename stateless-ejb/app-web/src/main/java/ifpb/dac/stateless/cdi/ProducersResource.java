b80b461c66da73479c900dcfb80659ad87bc5469
Isleimar Oliveira <isleimar@hotmail.com>
2019-05-30 10:28:11 -0300

    Primeiro commit

---
+++ stateless-ejb/app-web/src/main/java/ifpb/dac/stateless/cdi/ProducersResource.java
@@ -0,0 +1,20 @@
+package ifpb.dac.stateless.cdi;
+
+import ifpb.dac.stateless.IFCalculadora;
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+
+/**
+ * @author Ricardo Job
+ */
+@ApplicationScoped
+public class ProducersResource {
+    
+    @Produces 
+    @Resource(lookup = "java:global/stateless-ejb-core/CalculadoraImpl")
+    private IFCalculadora calculadora;
+
+}
